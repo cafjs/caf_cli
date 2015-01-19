39d45f773b5de292c41b55491118d995ae111768
Antonio Lain <antlai@cafjs.com>
2015-01-19 00:27:53 -0800

    Simplify error propagation

--- test/test-cli.js
+++ test/test-cli.js
@@ -1,5 +1,6 @@
 var async = require('async');
 var caf_comp = require('caf_components');
+var json_rpc = require('caf_transport').json_rpc;
 var myUtils = caf_comp.myUtils;
 var cli = require('../index.js');
 var hello = require('./hello/main.js');
@@ -29,7 +30,7 @@ module.exports = {
         if (!this.$) {
             cb(null);
         } else {
-            this.$.top.__ca_shutdown__(null, cb);
+            this.$.top.__ca_graceful_shutdown__(null, cb);
         }
     },
     hello: function(test) {
@@ -93,6 +94,71 @@ module.exports = {
                                 };
                                 s.close();
                             }
+                        ], function(err, res) {
+                            test.ifError(err);
+                            test.done();
+                        });
+    },
+    helloException: function(test) {
+        var self = this;
+        test.expect(11);
+        var s;
+        async.series([
+                         function(cb) {
+                             s = new cli.Session('ws://localhost:3000',
+                                                 'antonio-c1');
+                             s.onopen = function() {
+                                 var cb0 = function(err, val) {
+                                     console.log('ERROR: This should never' +
+                                                 'execute');
+                                     // add an extra test to fail test count
+                                     test.ok(err);
+                                     cb('Error: response instead of close');
+                                 };
+                                 s.helloException('foo', cb0);
+                             };
+                             s.onclose = function(err) {
+                                 test.ok(err);
+                                 test.ok(json_rpc.isSystemError(err));
+                                 test.equal(json_rpc
+                                                .getSystemErrorCode(err),
+                                            json_rpc.ERROR_CODES
+                                            .exceptionThrown);
+                                 var error = json_rpc
+                                     .getSystemErrorData(err);
+                                 test.equal(error.msg, 'helloException');
+                                 test.ok(error.stack);
+                                 cb(null,null);
+                             };
+                         },
+                         function(cb) {
+                             s = new cli.Session('ws://localhost:3000',
+                                                 'antonio-c1');
+                             s.onopen = function() {
+                                 var cb0 = function(err, val) {
+                                     console.log('ERROR: This should never' +
+                                                 'execute');
+                                     // add an extra test to fail test count
+                                     test.ok(err);
+                                     cb('Error: response instead of close');
+                                 };
+                                 s.helloDelayException('foo', cb0);
+                             };
+                             s.onclose = function(err) {
+                                 test.ok(err);
+                                 test.ok(json_rpc.isSystemError(err));
+                                 test.equal(json_rpc
+                                            .getSystemErrorCode(err),
+                                            json_rpc.ERROR_CODES
+                                            .exceptionThrown);
+                                 var error = json_rpc
+                                     .getSystemErrorData(err);
+                                 test.equal(error.msg,
+                                            'helloDelayException');
+                                 test.ok(error.stack);
+                                 cb(null,null);
+                             };
+                         }
                         ], function(err, res) {
                             test.ifError(err);
                             test.done();
